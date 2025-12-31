c14cbd0871ea2522585fa44af43f999ff47fb8c5
Harshit265 <harshitbhatpuria265@gmail.com>
2025-12-31 20:29:24 +0530

    Added profession selection and profile model

--- sickle_project/settings.py
+++ sickle_project/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'core',  # Add your app name here!
+]
